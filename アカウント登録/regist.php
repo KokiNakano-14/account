691176b2902b92d9820f26055aaea2637e86facc
kokinakano-14 <nakanokoki14@gmail.com>
2023-09-07 01:15:51 +0900

    regist_phpを作成中

---
+++ アカウント登録/regist.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>アカウント登録</title>
+</head>
+<body>
+
+  <div class="header">
+    <p>ナビゲーションバー</p>
+  </div>
+
+  <h1>アカウント登録画面</h1>
+  <form method="post" action="regist_conplete.php">
+    <div>
+      <label>名前（性）</label>
+      <input type="text" class="text" size="20" maxlength="10" name="family_name">
+    </div>
+  </form>
+</body>
+</html>
