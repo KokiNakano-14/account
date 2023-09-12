fff81be591b9b82cd91c92f1cf522688e790f724
kokinakano-14 <nakanokoki14@gmail.com>
2023-09-13 00:40:42 +0900

    次回はDBとの繋がりを確認

--- client_account/regist.php
+++ client_account/regist.php
@@ -13,13 +13,13 @@
 <body>
 
 
-<!-- <?php  /* PDO文の作成 */
+<?php  /* PDO文の作成 */
 
 mb_internal_encoding("utf8");
 $pdo = new PDO("mysql:dbname=client; host=localhost;","root","nako14");
 $stmt = $pdo -> query("select * from user_info");
 
-?> -->
+?>
 
 <!-- ナビゲーションバー -->
   <div class="header">
@@ -56,6 +56,7 @@ $stmt = $pdo -> query("select * from user_info");
         <input type="password" class="password" size="20" maxlength="10" name="password">
       </div>
       <div>
+        <!-- 性別判別のjavascriptを作成 -->
         <label>性別</label>
         <input type="radio" class="radio" value="男" name="gender">男
         <input type="radio" class="radio" value="女" name="gender">女
