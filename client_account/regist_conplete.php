8ed228e2deef6f60f6a1d2cef3486a200ac763e0
kokinakano-14 <nakanokoki14@gmail.com>
2023-09-28 17:35:01 +0900

    cssでデザイン構築中

--- client_account/regist_conplete.php
+++ client_account/regist_conplete.php
@@ -8,3 +8,30 @@ $stmt = $pdo -> query("select * from user_info");
 $pdo -> exec("insert into user_info(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority) values('".$_POST['family_name']."','".$_POST['last_name']."','".$_POST['family_name_kana']."','".$_POST['last_name_kana']."','".$_POST['mail']."','".$_POST['password']."','".$_POST['gender']."','".$_POST['postal_code']."','".$_POST['prefecture']."','".$_POST['address_1']."','".$_POST['address_2']."','".$_POST['authority']."');");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="conplete.css">
+  <title>アカウント登録</title>
+</head>
+<body>
+  <div class="header">ナビゲーションバー</div>
+
+  <h1>アカウント登録完了画面</h1>
+
+  <div class="main">
+    <p>登録完了しました</p>
+  </div>
+<!-- regist.phpに戻る -->
+  <form action="regist.php">
+    <div class="back_btn">
+      <input type="submit" class="button" value="TOPページに戻る">
+    </div>
+  </form>
+
+  <div class="footer">フッター</div>
+</body>
+</html>
