c706d6d97becef9b55383f65cfccfcf9d1dd5674
kokinakano-14 <nakanokoki14@gmail.com>
2023-10-12 23:33:22 +0900

    入力値の保持作成済み

--- client_account/regist.php
+++ client_account/regist.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -22,44 +21,58 @@
       <!-- patternでひらがな、漢字指定 -->
       <div>
         <label>名前（性）</label>
-        <input type="text" class="text" size="20" maxlength="10"  name="family_name" required
+        <input type="text" class="text" size="20" maxlength="10" name="family_name"
+          required
           oninvalid="this.setCustomValidity('名前（性）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          pattern="[ぁ-んァ-ン一-龯]+"
+          value="<?php if( !empty($_POST['family_name']) ){ echo $_POST['family_name']; } ?>">
       </div>
       <!-- patternでひらがな、漢字指定 -->
       <div>
         <label>名前（名）</label>
-        <input type="text" class="text" size="20" maxlength="10" name="last_name" required
+        <input type="text" class="text" size="20" maxlength="10" name="last_name" 
+          required
           oninvalid="this.setCustomValidity('名前（名）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          pattern="[ぁ-んァ-ン一-龯]+"
+          value="<?php if( !empty($_POST['last_name']) ){ echo $_POST['last_name']; } ?>">
       </div>
       <!-- patternでカタカナのみ指定 -->
       <div>
         <label>カナ（性）</label>
-        <input type="text" class="text"  size="20" maxlength="10" name="family_name_kana" required
+        <input type="text" class="text"  size="20" maxlength="10" name="family_name_kana" 
+          required
           oninvalid="this.setCustomValidity('カナ（性）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+
+          value="<?php if( !empty($_POST['family_name_kana']) ){ echo $_POST['family_name_kana']; } ?>">
       </div>
       <!-- patternでカタカナのみ指定 -->
       <div>
         <label>カナ（名）</label>
-        <input type="text" class="text"  size="20" maxlength="10" name="last_name_kana" required
+        <input type="text" class="text"  size="20" maxlength="10" name="last_name_kana"
+          required
           oninvalid="this.setCustomValidity('カナ（名）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+        
+          value="<?php if( !empty($_POST['last_name_kana']) ){ echo $_POST['last_name_kana']; } ?>">
       </div>
       <!-- type="email"でメール指定 -->
       <div>
         <label>メールアドレス</label>
         <input type="email" class="mail"  size="20" maxlength="100" name="mail" required
           oninvalid="this.setCustomValidity('メールアドレスが未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          value="<?php if( !empty($_POST['mail']) ){ echo $_POST['mail']; } ?>">
       </div>
       <!-- 半角英数字指定 -->
       <div>
         <label>パスワード</label>
         <input type="password" class="password" size="20" maxlength="10" name="password" required
           oninvalid="this.setCustomValidity('パスワードが未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          value="<?php if( !empty($_POST['password']) ){ echo $_POST['password']; } ?>">
       </div>
       <div>
         <label>性別</label>
@@ -71,74 +84,52 @@
         <label>郵便番号</label>
         <input type="text" class="text" name="postal_code" maxlength="7" required
           oninvalid="this.setCustomValidity('郵便番号が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          
+          value="<?php if( !empty($_POST['postal_code']) ){ echo $_POST['postal_code']; } ?>">
       </div>
       <div>
         <label>都道府県（住所）</label>
         <select name="prefecture" required
           oninvalid="this.setCustomValidity('都道府県が未選択です')"
           onchange="this.setCustomValidity('')">
+
           <option value="" selected>選択してください</option>
-          <option value="1">北海道</option>
-          <option value="2">青森県</option>
-          <option value="3">岩手県</option>
-          <option value="4">宮城県</option>
-          <option value="5">秋田県</option>
-          <option value="6">山形県</option>
-          <option value="7">福島県</option>
-          <option value="8">茨城県</option>
-          <option value="9">栃木県</option>
-          <option value="10">群馬県</option>
-          <option value="11">埼玉県</option>
-          <option value="12">千葉県</option>
-          <option value="13">東京都</option>
-          <option value="14">神奈川県</option>
-          <option value="15">新潟県</option>
-          <option value="16">富山県</option>
-          <option value="17">石川県</option>
-          <option value="18">福井県</option>
-          <option value="19">山梨県</option>
-          <option value="20">長野県</option>
-          <option value="21">岐阜県</option>
-          <option value="22">静岡県</option>
-          <option value="23">愛知県</option>
-          <option value="24">三重県</option>
-          <option value="25">滋賀県</option>
-          <option value="26">京都府</option>
-          <option value="27">大阪府</option>
-          <option value="28">兵庫県</option>
-          <option value="29">奈良県</option>
-          <option value="30">和歌山県</option>
-          <option value="31">鳥取県</option>
-          <option value="32">島根県</option>
-          <option value="33">岡山県</option>
-          <option value="34">広島県</option>
-          <option value="35">山口県</option>
-          <option value="36">徳島県</option>
-          <option value="37">香川県</option>
-          <option value="38">愛媛県</option>
-          <option value="39">高知県</option>
-          <option value="40">福岡県</option>
-          <option value="41">佐賀県</option>
-          <option value="42">長崎県</option>
-          <option value="43">熊本県</option>
-          <option value="44">大分県</option>
-          <option value="45">宮崎県</option>
-          <option value="46">鹿児島県</option>
-          <option value="47">沖縄県</option>
+          
+          <!-- prefecturesに都道府県の配列を入れる。連想配列を利用する -->
+          <?php
+            $prefectures = [
+              "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+              "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
+              "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", 
+              "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県",
+              "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+              "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
+              "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
+            ];
+
+            // asで&prefecturesの中の全て
+            foreach ($prefectures as $prefecture) {
+              echo "<option value=\"$prefecture\">$prefecture</option>";
+            }
+          ?>
         </select>
       </div>
       <div>
         <label>住所（市区町村）</label>
         <input type="text" class="text" size="20" maxlength="10" name="address_1" required
           oninvalid="this.setCustomValidity('住所（市町村）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          pattern="[^\x21-\x2C\x2E\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]*"
+          value="<?php if( !empty($_POST['address_1']) ){ echo $_POST['address_1']; } ?>">
       </div>
       <div>
         <label>住所（番地）</label>
         <input type="text" class="text" size="20" maxlength="100" name="address_2" required
           oninvalid="this.setCustomValidity('住所（番地）が未入力です')"
-          onchange="this.setCustomValidity('')">
+          onchange="this.setCustomValidity('')"
+          pattern="[^\x21-\x2C\x2E\x2F\x3A-\x40\x5B-\x60\x7B-\x7E]*"
+          value="<?php if( !empty($_POST['address_2']) ){ echo $_POST['address_2']; } ?>">
       </div>
       <div>
         <label>アカウント権限</label>
