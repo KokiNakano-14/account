c191a1d6a82344179a6585350e95203af7ee1a26
kokinakano-14 <nakanokoki14@gmail.com>
2023-10-23 23:40:22 +0900

    全体の構成を変更中

--- client_account/regist.php
+++ client_account/regist.php
@@ -15,7 +15,9 @@
 <body>
 
   <!-- ヘッダー -->
-  <header>ナビゲーションバー</header>
+  <header>
+    <p>ナビゲーションバー</p>
+  </header>
 
   <!-- 入力フォーム -->
   <main>
@@ -24,57 +26,57 @@
 
     <form method="$_POST" action="regist_conform.php">
       <!-- 以下情報を入力 -->
-      <div>
+      <div class="nyuuryoku">
         <label>名前（性）</label>
         <input type="text" class="name1" maxlength="10" name="family_name">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>名前（名）</label>
         <input type="text" class="name2" maxlength="10" name="last_name">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>カナ（性）</label>
         <input type="text" class="name3" maxlength="10" name="family_name_kana">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>カナ（名）</label>
         <input type="text" class="name4" maxlength="10" name="last_name_kana">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>メールアドレス</label>
         <input type="email" class="mail" maxlength="100" name="mail">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>パスワード</label>
         <input type="text" class="password" maxlength="10" name="password">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>性別</label>
         <input type="radio" class="radio1" value="XXX" name="gender" checked>男
         <input type="radio" class="radio2" value="XXX" name="gender">女
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>郵便番号</label>
         <input type="text" class="postal" maxlength="7" name="postal_code">
       </div>
 
-      <div>
+      <div class="nyuuryoku">
         <label>都道府県（住所）</label>
-        <select name="prefecture">
+        <select class="prefecture" name="prefecture">
 
           <option value="">選択してください</option>
 
           <!-- $prefectureに都道府県の配列を格納する -->
           <?php
 
-            $prefecture = [
+            $prefectures = [
               "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
               "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
               "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", 
@@ -84,16 +86,42 @@
               "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
             ];
 
-            
-
+            // asで$prefectureの中全て選択
+            foreach ($prefectures as $prefecture) {
+              echo "<option value=\"$prefecture\">$prefecture</option>";
+            }
           ?>
 
         </select>
       </div>
+
+      <div class="nyuuryoku">
+        <label>住所（市区町村）</label>
+        <input type="text" class="address1" maxlength="10" name="address_1">
+      </div>
+
+      <div class="nyuuryoku">
+        <label>住所（番地）</label>
+        <input type="text" class="address2" maxlength="100" name="address_2">
+      </div>
+
+      <div class="nyuuryoku">
+        <label>アカウント権限</label>
+        <select class="authority" name="authority">
+          <option value="0">一般</option>
+          <option value="1">管理者</option>
+        </select>
+      </div>
+
+      <div>
+        <input type="submit" class="kakunin_btn" value="確認する">
+      </div>
     </form>
   </main>
 
-  <footer>フッター</footer>
+  <footer>
+    <p>フッター</p>
+  </footer>
 
 </body>
 </html>l
--- client_account/regist_conplete.php
+++ client_account/regist_conplete.php
@@ -7,7 +7,10 @@ $stmt = $pdo->query("select * from user_info");
 // DBname=clientに繋ぐためのコード
 $pdo->exec("insert into user_info(family_name,last_name,family_name_kana,last_name_kana,mail,password,gender,postal_code,prefecture,address_1,address_2,authority) values('" . $_POST['family_name'] . "','" . $_POST['last_name'] . "','" . $_POST['family_name_kana'] . "','" . $_POST['last_name_kana'] . "','" . $_POST['mail'] . "','" . $_POST['password'] . "','" . $_POST['gender'] . "','" . $_POST['postal_code'] . "','" . $_POST['prefecture'] . "','" . $_POST['address_1'] . "','" . $_POST['address_2'] . "','" . $_POST['authority'] . "');");
 
-var_dump($user_password);
+$user_password = $_POST['password'];
+// パスワードをハッシュ化する
+$hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
+// $hashed_passwordをpasswordに代入させる
 ?>
 
 <!DOCTYPE html>
