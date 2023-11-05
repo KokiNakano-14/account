22ef8fd75ea71f87892111fdd1f1d268b79514e9
kokinakano-14 <nakanokoki14@gmail.com>
2023-11-05 21:29:01 +0900

    削除フラグ更新済み

--- client_account/regist_conplete.php
+++ client_account/regist_conplete.php
@@ -1,10 +1,11 @@
 <?php
-// PDO文の作成
 
+// PDO文の作成
 mb_internal_encoding("utf8");
 $pdo = new PDO("mysql:dbname=client; host=localhost;", "root", "nako14");
 $stmt = $pdo->query("select * from user_info");
 
+// パスワードのハッシュ化
 $user_password = $_POST['password'];
 $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
 
