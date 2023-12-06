04cb38b394135fb3b1d78bc8611b700c6756f24c
kokiNakano14 <nakanokoki14@gmail.com>
2023-12-06 23:14:52 +0900

    テーブル作成中

--- client_account/list/list.php
+++ client_account/list/list.php
@@ -11,7 +11,17 @@
     <div class="haeder">ヘッダー</div>
   </header>
 
-  <main></main>
+  <main>
+
+  <table>
+    <tbody>
+      <tr>
+        <th>ID</th><th>名前（性）</th><th>名前（名）</th><th>カナ（性）</th><th>カナ（名）</th><th>メールアドレス</th><th>性別</th><th>アカウント権限</th><th>削除フラグ</th><th>登録日時</th><th>操作</th>
+      </tr>
+    </tbody>
+  </table>
+
+  </main>
 
   <footer>
     <div class="footer">フッター</div>
