687fd0b4ead691abbaaa7b1bcf2d1e3319c292c5
kokiNakano14 <nakanokoki14@gmail.com>
2023-12-12 23:30:08 +0900

    アカウントリスト作成中

--- client_account/list/list.php
+++ client_account/list/list.php
@@ -1,3 +1,14 @@
+<?php
+  // 文字化け防止のコード
+  mb_internal_encoding("utf8");
+  // DBに接続する
+  $pdo = new PDO("mysql:dbname=client;host=localhost;","root","nako14");
+  // order by id desc でidを下から表示
+  $stmt = $pdo -> query("select * from user_info order by id desc");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -18,6 +29,10 @@
       <tr>
         <th>ID</th><th>名前（性）</th><th>名前（名）</th><th>カナ（性）</th><th>カナ（名）</th><th>メールアドレス</th><th>性別</th><th>アカウント権限</th><th>削除フラグ</th><th>登録日時</th><th>操作</th>
       </tr>
+      <tr>
+        <th><?php echo"id"; ?></th>
+      </tr>
+      
     </tbody>
   </table>
 
